b9c9699d8b154aa3d4483402990f8a0e1fb98672
JunganBae <jungg0030@snu.ac.kr>
2025-06-23 15:26:39 +0900

    ff
    ff

--- real proj2/script.js
+++ real proj2/script.js
@@ -1,15 +1,13 @@
-// script.js (메인 화면용 스크립트)
+// script.js (메인 페이지용)
 
 document.addEventListener('DOMContentLoaded', () => {
     const pastaItems = document.querySelectorAll('.pasta-item');
 
     pastaItems.forEach(item => {
         item.addEventListener('click', () => {
-            const pastaType = item.dataset.type; // data-type 속성에서 파스타 종류 가져오기
-            if (pastaType) {
-                // pasta_timer.html로 이동하면서 파스타 종류를 URL 파라미터로 넘김
-                window.location.href = `pasta_timer.html?type=${pastaType}`;
-            }
+            const pastaType = item.dataset.type; // data-type 속성 값 가져오기
+            // 타이머 페이지로 이동하며 파스타 타입을 URL 파라미터로 전달
+            window.location.href = `pasta_timer.html?type=${pastaType}`;
         });
     });
 });
